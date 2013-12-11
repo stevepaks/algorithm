544c888afd65c51705cc305fe78ddbb4b870a669
jonghunpark <stevepaks@naver.com>
2013-12-11 12:14:41 +0900

    remove algorithm

--- algorithm/slicepaper/PaperStore.java
+++
@@ -1,8 +0,0 @@
-package slicedPaper;
-
-public interface PaperStore {	
-	public void createPaper(int widthStartPoint, int widthEndPoint, int heightStartPoint, int heightEndPoint);	
-	public PaperFactory getPaperFactory();	
-	public Paper getPaper(); 
-
-}
