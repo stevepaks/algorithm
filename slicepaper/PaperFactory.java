33f92990948598d3bde5c60efecbb877578695f1
stevepaks <stevepaks@naver.com>
2013-12-11 21:44:33 +0900

    remove directory

--- slicepaper/PaperFactory.java
+++
@@ -1,9 +0,0 @@
-package slicedPaper;
-
-import java.util.ArrayList;
-
-public interface PaperFactory {
-	public Paper createPaper(int widthStartPoint, int widthEndPoint, int heightStartPoint, int heightEndPoint);	
-	public ArrayList<Paper> slicePaper(int isWidth, int point, ArrayList<Paper> slicedPapers);
-
-}
